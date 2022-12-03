1556452c16b8c40fa49bc88b309fa1c51e642551
CFS <2408871548@qq.com>
2022-12-03 22:38:14 +0800

    feat(1):加上qchar的版本

--- showmessage.cpp
+++ showmessage.cpp
@@ -2,6 +2,11 @@
 #include "ui_showmessage.h"
 #include <QCloseEvent>
 #include <QScreen>
+#include <QRandomGenerator>
+#include <ctime>
+#include <QLineSeries>
+#include <QValueAxis>
+#include <QTime>
 showMessage::showMessage(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::showMessage)
@@ -10,16 +15,25 @@ showMessage::showMessage(QWidget *parent) :
     connect(ui->pushButton, &QPushButton::pressed, this, &showMessage::refresh);
     connect(ui->pushButton_2, &QPushButton::pressed, this, &showMessage::refreshFourSecond);
     this->setWindowFlags(Qt::WindowStaysOnTopHint | Qt::WindowCloseButtonHint);
+    m_dataTable = generateRandomData(m_listCount, m_valueMax, m_valueCount);
+    QChartView *chartView;
+
+    chartView = new QChartView(createLineChart());
+    ui->horizontalLayout->addWidget(chartView);
+    ui->horizontalLayout->setContentsMargins(0, 0, 0, 0);
+    ui->verticalLayout->setContentsMargins(0, 0, 0, 0);
+    ui->horizontalLayout_2->setContentsMargins(0, 0, 0, 8);
 
     QScreen *screen = qApp->primaryScreen();
     int widthMax = screen->size().width() / 3;            //屏幕宽
     int heightMax = screen->size().height() / 3;
-    int widthMin = screen->size().width() / 6;            //屏幕宽
-    int heightMin = screen->size().height() / 6;
+    int widthMin = 320;            //屏幕宽
+    int heightMin = 319;
 
     this->setMaximumSize(widthMax, heightMax);
     this->setMinimumSize(widthMin, heightMin);
     this->resize(widthMin, heightMin);
+
    // this->setSize(widthMin, heightMin);
    // this->overrideWindowFlags(Qt::WindowStaysOnTopHint);
 }
@@ -37,8 +51,19 @@ void showMessage::closeEvent(QCloseEvent *e)
 
 void showMessage::refresh()
 {
-    auto time = clock();
-    ui->lineEdit->setText(QString::number(time));
+    QTime current_time = QTime::currentTime();
+
+    int hour = current_time.hour();        //当前的小时
+    int minute = current_time.minute();    //当前的分
+    int second = current_time.second();    //当前的秒
+    int msec = current_time.msec();        //当前的毫秒
+
+    time_t now = time(0);
+    qDebug() << hour << " " << minute << " " << second << " " << msec;
+    series->setName("yuyonmgsheng");
+    // 把 now 转换为字符串形式
+  //  char* dt = ctime(&now);
+   // ui->lineEdit->setText(QString(dt));
 }
 
 
@@ -56,11 +81,11 @@ void showMessage::mousePressEvent(QMouseEvent *event)
        mousePoint = event->globalPos() - this->pos();
        event->accept();
    }
-//   else if(event->button() == Qt::RightButton){
-//        //如果是右键
-//        this->close();
+   else if(event->button() == Qt::RightButton){
+        //如果是右键
+        qApp->quit();
 
-//    }
+    }
 }
 void showMessage::mouseMoveEvent(QMouseEvent *event)
 {
@@ -75,4 +100,60 @@ void showMessage::mouseMoveEvent(QMouseEvent *event)
 void showMessage::mouseReleaseEvent(QMouseEvent *event)
 {
     mouse_press = false;
+}
+
+DataTable showMessage::generateRandomData(int listCount, int valueMax, int valueCount)
+{
+    DataTable dataTable;
+
+    // generate random data
+    for (int i(0); i < listCount; i++) {
+        DataList dataList;
+        qreal yValue(0);
+        for (int j(0); j < valueCount; j++) {
+            yValue = yValue + QRandomGenerator::global()->bounded(valueMax / (qreal) valueCount);
+            QPointF value((j + QRandomGenerator::global()->generateDouble()) * ((qreal) m_valueMax / (qreal) valueCount),
+                          yValue);
+            QString label = "Slice " + QString::number(i) + ":" + QString::number(j);
+            dataList << Data(value, label);
+        }
+        dataTable << dataList;
+    }
+
+    return dataTable;
+}
+
+QChart *showMessage::createLineChart()
+{
+    //![1]
+    QChart *chart = new QChart();
+    chart->setTitle("Line chart");
+    //![1]
+
+    //![2]
+    QString name("Series ");
+    int nameIndex = 0;
+    for (const DataList &list : m_dataTable) {
+        series = new QLineSeries(chart);
+        for (const Data &data : list)
+            series->append(data.first);
+        series->setName(name + QString::number(nameIndex));
+        nameIndex++;
+        chart->addSeries(series);
+    }
+    //![2]
+
+    //![3]
+    chart->createDefaultAxes();
+    chart->axes(Qt::Horizontal).first()->setRange(0, m_valueMax);
+    chart->axes(Qt::Vertical).first()->setRange(0, m_valueCount);
+    //![3]
+    //![4]
+    // Add space to label to add space between labels and axis
+    QValueAxis *axisY = qobject_cast<QValueAxis*>(chart->axes(Qt::Vertical).first());
+    Q_ASSERT(axisY);
+    axisY->setLabelFormat("%.1f  ");
+    //![4]
+
+    return chart;
 }
--- showmessage.h
+++ showmessage.h
@@ -3,9 +3,15 @@
 
 #include <QWidget>
 #include <QMouseEvent>
+#include <QChart>
+#include <QChartView>
 namespace Ui {
 class showMessage;
 }
+class QLineSeries;
+typedef QPair<QPointF, QString> Data;
+typedef QList<Data> DataList;
+typedef QList<DataList> DataTable;
 
 class showMessage : public QWidget
 {
@@ -19,10 +25,19 @@ private slots:
     void refresh();
     void refreshFourSecond();
 protected:
+    DataTable generateRandomData(int listCount, int valueMax, int valueCount);
+    QChart *createLineChart();
     void mouseMoveEvent(QMouseEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
 private:
+    QLineSeries *series = nullptr;
+    int m_listCount{1};
+    int m_valueMax{24};
+    int m_valueCount{7};
+    QList<QChartView *> m_charts;
+    DataTable m_dataTable;
+
     QPoint mousePoint;
     bool mouse_press;
 private:
